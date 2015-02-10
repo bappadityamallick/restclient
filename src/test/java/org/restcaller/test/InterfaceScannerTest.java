7d7186964108066cfb79e8f20edb4d46d70bad3f
kumar.sambhav.jain@gmail.com <kumar.sambhav.jain@gmail.com>
2015-02-10 21:25:05 +0530

    Added test folder and properties file.

---
+++ src/test/java/org/restcaller/test/InterfaceScannerTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.restcaller.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Kumar Sambhav Jain
+ *
+ */
+public class InterfaceScannerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
